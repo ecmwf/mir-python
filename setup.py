ed0ef0587730c5df62c1fa7cf12dd096a7f77dfb
Olivier Iffrig <olivier.iffrig@ecmwf.int>
2025-01-29 11:24:42 +0000

    Make build dependencies explicit

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 import os
-from distutils.core import setup
-from distutils.extension import Extension
 
 from Cython.Build import cythonize
-from Cython.Distutils import build_ext
+from setuptools import Extension, setup
 
 home = os.environ.get("HOME")
 source = os.environ.get(
